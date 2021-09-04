3ba69602566883b6d3ec1dae62693ccfbce8c444
Mohammad Hossein Safari <73357520+MHsafari@users.noreply.github.com>
2021-09-05 01:21:16 +0430

    Update make_json.py

--- make_json.py
+++ make_json.py
@@ -2,20 +2,20 @@ import json
 
 with open("data.json", "w") as json_data:
     data = {
-        "my_token": "1815804020:AAHbxfIi2I1CQaylQ-Pjp4jIPosQAAECEkk",
-        "chat_id": "-1001572436793",
+        "my_token": "yyyyyyyyyy",
+        "chat_id": "yyyyyyyyyyy",
         
-        "my_token_car": "1989284238:AAHIPSYCi0gSeXZtVTsSSJ7v3mL2lGcbhZU",
-        "chat_id_car": "-1001524790543",
+        "my_token_car": "yyyyyyyyyyy",
+        "chat_id_car": "-yyyyyyyyyyy",
         
-        "my_token_mobile":"1963376161:AAEc4OZZC9unjueyzzEQPDexBDDFatBZeSk",
-        "chat_id_mobile":"-1001522971851",
+        "my_token_mobile":"yyyyyyyyyyy:yyyyyyyyyyy",
+        "chat_id_mobile":"-yyyyyyyyyyy",
         
-        "my_token_rent":"1996090629:AAHYfJK2HTz4lNn9yvXzXK7v0AWQczL7CAE",
-        "chat_id_rent":"-1001175982650",
+        "my_token_rent":"yyyyyyyyyyy:yyyyyyyyyyy",
+        "chat_id_rent":"-yyyyyyyyyyy",
         
-        "my_token_sell":"1982231457:AAHBPWQ_8YpjHLB8mG6zEiqg6C3qw7Bp5VE",
-        "chat_id_sell":"-1001336474001",
+        "my_token_sell":"yyyyyyyyyyy:yyyyyyyyyyy",
+        "chat_id_sell":"-yyyyyyyyyyy",
         
         
         
